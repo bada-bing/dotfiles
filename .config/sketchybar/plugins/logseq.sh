68cc7eead98cc05c3100b62e91f8574eebbca1b9
Miki <bada.bing.bing@live.com>
2025-11-23 14:57:16 +0100

    ♻️ [sketchybar] re-enable logseq item

--- .config/sketchybar/plugins/logseq.sh
+++ .config/sketchybar/plugins/logseq.sh
@@ -7,13 +7,6 @@
 
 # To make the logseq page dynamic, check how it is done in wa-2/logseq-sessionizer 
 
-RESULT=$(curl --request POST \
-  --url http://127.0.0.1:12315/api \
-  --header 'authorization: Bearer d3183aef-d30a-4936-a370-018ee1b425ed' \
-  --header 'content-type: application/json' \
-  --header 'user-agent: vscode-restclient' \
-  --data '{"method": "logseq.db.q", "args": ["(and (task NOW) [[WPR-16235-introduce-scripting-integration-in-cart-ui-next]])"]}' | jq '.[0].content')
+TASK=$(sh ~/src/scripts/local_development/get_logseq_today_active_task.sh | tr -d '"')
 
-RESULT=$(echo "$RESULT" | tr -d '"')
-
-sketchybar --set "$NAME" label="$RESULT" icon="$($CONFIG_DIR/plugins/icon_map_fn.sh "Logseq")"
+sketchybar --set "$NAME" label="$TASK" icon="$($CONFIG_DIR/plugins/icon_map_fn.sh "Logseq")"
