2ba7960fe4a0ba7cd7f8bb4461597690b15cd321
Miki <bada.bing.bing@live.com>
2026-05-20 11:28:46 +0200

    🚛 [bootstrap] Convert defaults_list to script to simplify maintenance

--- _bootstrap/set_macos_defaults.sh
+++ _bootstrap/set_macos_defaults.sh
@@ -1,15 +1,24 @@
-# Finder
+#!/bin/bash
+
+# --- macOS Defaults ---
+
+# Finder: Show hidden files, show all extensions, use column view, show path bar, and sort folders first.
 defaults write com.apple.finder AppleShowAllFiles -bool true
 defaults write NSGlobalDomain AppleShowAllExtensions -bool true
 defaults write com.apple.Finder FXPreferredViewStyle clmv
 defaults write com.apple.finder ShowPathbar -bool true
 defaults write com.apple.finder _FXSortFoldersFirst -bool true
 
-# Dock
+# Dock: Enable autohide, remove recent apps, and disable MRU spaces.
 defaults write com.apple.dock autohide -bool true
 defaults write com.apple.dock show-recents -bool false
 defaults write com.apple.dock "mru-spaces" -bool false
 
-# Mouse & Trackpad
+# Mouse & Trackpad: Set scaling.
 defaults write com.apple.mouse.scaling -float 3.0
 defaults write com.apple.trackpad.scaling -float 3.0
+
+# Kill affected applications to apply changes
+for app in "Finder" "Dock"; do
+  killall "$app" &> /dev/null
+done
